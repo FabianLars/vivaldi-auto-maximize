75c62926e4ef36bb8472cb31a20848cc5da71221
FabianLars <fabianlars@fabianlars.de>
2021-05-06 03:35:14 +0200

    hopefully fix registration after inactivity

--- background.js
+++ background.js
@@ -1,37 +1,25 @@
-// Version 1 (add FocusListener on onCreated and remove it directly after resizing once)
-// DOESN'T WORK AFTER SERVICE WORKER GOES INACTIVE
-/* function focusListener(windowId) {
-    if (windowId > 0) {
-        chrome.windows.get(windowId).then(window => {
-            if (window.type === 'normal') {
-                chrome.windows.update(windowId, { state: 'maximized' });
-                chrome.windows.onFocusChanged.removeListener(focusListener);
-            }
-        });
-    }
-}
-
-chrome.windows.onCreated.addListener(() => {
-    chrome.windows.onFocusChanged.addListener(focusListener);
-}); */
-
-// Version 2 //
 // Check on every focus change.
 // Resize every window only once by keeping track of already resized windowIds
 // windowIds are unique per browser session
-chrome.windows.onFocusChanged.addListener(async windowId => {
+
+// This looks ugly but everything else (that seemed to work in other browsers) crashed on vivaldi
+chrome.windows.onFocusChanged.addListener(windowId => {
     if (windowId > 0) {
-        let wasMaxed = await chrome.storage.local.get('w' + windowId);
-        if (!wasMaxed) {
-            let window = await chrome.windows.get(windowId);
-            if (window.type === 'normal') {
-                chrome.windows.update(windowId, { state: 'maximized' });
-                await chrome.storage.local.set({ ['w' + windowId]: true });
+        const key = 'w' + windowId;
+        chrome.storage.local.get(key, wasMaxed => {
+            if (!wasMaxed || !wasMaxed[key]) {
+                chrome.windows.update(windowId, { state: 'maximized' }, () => {
+                    chrome.storage.local.set({ [key]: true }, () => {
+                        /* Empty callback function, because wtf */
+                    });
+                });
             }
-        }
+        });
     }
 });
 
-chrome.windows.onRemoved.addListener(async windowId => {
-    await chrome.storage.local.remove('w' + windowId);
+chrome.windows.onRemoved.addListener(windowId => {
+    chrome.storage.local.remove('w' + windowId, () => {
+        /* Empty callback function, because wtf */
+    });
 });
